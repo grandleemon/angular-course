e2a83ab5fa6634ab65133acb59aa6a5d2360d469
Max <zeetreex@gmail.com>
2022-07-26 16:22:54 +0300

    passed a data to a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {IProduct} from "./models/product.interface";
+import {products} from './data/products'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular';
+	title = 'angular';
+
+	products: IProduct[] = products
 }
