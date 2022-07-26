6eb05c076f1d7db9e998a9c77369e09c19ad3217
Max <zeetreex@gmail.com>
2022-07-26 17:17:06 +0300

    ngOnInit request, loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProduct} from "./models/product.interface";
-import {products} from './data/products'
+import {ProductsService} from "./services/products.service";
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-	title = 'angular';
+export class AppComponent implements OnInit {
+	constructor(private readonly productService: ProductsService) {
+	}
 
-	products: IProduct[] = products
+	title = 'angular';
+	products: IProduct[] = []
+	loading = false
+
+	async ngOnInit(): Promise<void> {
+		this.loading = true;
+		(await this.productService.getAll()).subscribe(products => {
+			this.products = products
+			this.loading = false;
+		})
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {ProductComponent} from "./components/product/product.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ProductComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
